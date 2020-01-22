d156692721137410790b7312d006eb39066fe585
Chris Rabl <rabl.christopher@gmail.com>
2020-01-21 20:43:16 -0700

    Initial Commit

---
+++ Shadowbox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Shadowbox
+//
+//  Created by Chris Rabl on 1/21/20.
+//  Copyright © 2020 Coulee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
